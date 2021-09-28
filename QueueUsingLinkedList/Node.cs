994113ce78c106d01869b72564dc5c8c64456e56
zeenat08 <zeenatkhanbfs@gmail.com>
2021-09-29 00:40:54 +0530

    ZEENAT:[Added] node class

---
+++ QueueUsingLinkedList/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.QueueUsingLinkedList1
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int data)
+        {
+            this.data = data;
+        }
+    }
+}
