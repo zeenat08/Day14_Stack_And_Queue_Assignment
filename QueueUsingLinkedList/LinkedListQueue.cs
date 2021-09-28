2f670e3dc4ed1c571bbb74e1723b9af12adcc879
zeenat08 <zeenatkhanbfs@gmail.com>
2021-09-29 00:39:45 +0530

    ZEENAT:[Added] Using Linked List Creating Queue and Operating Queue

---
+++ QueueUsingLinkedList/LinkedListQueue.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.QueueUsingLinkedList1
+{
+    class LinkedListQueue
+    {
+
+
+        Node head = null;
+
+
+
+
+
+        internal void Enqueue(int data)
+
+
+        {
+
+
+            Node node = new Node(data);
+
+
+            if (head == null)
+
+
+                head = node;
+
+
+            else
+
+
+            {
+
+
+                Node temp = head;
+
+
+                while (temp.next != null)
+
+
+                {
+
+
+                    temp = temp.next;
+
+
+                }
+
+
+                temp.next = node;
+
+
+                //node.next = head;
+
+
+
+
+
+
+
+            }
+
+
+            Console.WriteLine("{0} inserted into queue ", node.data);
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+
+        internal void Display()
+
+
+        {
+
+
+            Node temp = this.head;
+
+
+            if (temp == null)
+
+
+            {
+
+
+                Console.WriteLine("Queue is empty");
+
+
+                return;
+
+
+
+
+
+            }
+
+
+            while (temp != null)
+
+
+            {
+
+              Console.Write(temp.data + " ");
+                temp = temp.next;
+
+
+            }
+
+
+        }
+    }
+}
