1df5ed8cfa795fe840bcf4ad59389a28cc72fee7
zeenat08 <zeenatkhanbfs@gmail.com>
2021-09-29 00:43:18 +0530

    ZEENAT:[Added] Creating Stack and Operating Stack Using Linked List

---
+++ StackUsingLinkedList/LinkedListStack.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.StackUsingLinkedList
+{
+    class LinkedListStack
+    {
+        private Node top;
+
+
+            public LinkedListStack()
+
+
+            {
+
+
+                this.top = null;
+
+
+            }
+
+
+
+
+
+
+
+            //30
+
+
+            internal void Push(int value)
+
+
+            {
+
+
+                Node node = new Node(value);
+
+
+                if (this.top == null)
+
+
+                    node.next = null;
+
+
+                else
+
+
+                    node.next = this.top;
+
+
+                this.top = node;
+
+
+                Console.WriteLine("{0} pushed to stack ", value);
+
+
+            }
+
+
+            public void Display()
+
+
+            {
+
+
+                Node temp = this.top;
+
+
+                while (temp != null)
+
+
+                {
+
+
+                    Console.Write(temp.data + " ");
+
+
+                    temp = temp.next;//1400
+
+
+                }
+
+
+            }
+
+
+
+
+
+
+
+            internal void Peek()
+
+
+            {
+
+
+                if (this.top == null)
+
+
+                {
+
+
+                    Console.WriteLine("Stack is empty");
+
+
+                    return;
+
+
+                }
+
+
+                Console.WriteLine("{0} is in the top of the stack ", this.top.data);//NullrefrenceRxception
+
+
+            }
+
+
+
+
+
+
+
+            internal void Pop()
+
+
+            {
+
+
+                if (this.top == null)
+
+
+                {
+
+
+                    Console.WriteLine("Stack is empty, Deletion is not possible");
+
+
+                    return;
+
+
+                }
+
+
+                Console.WriteLine("Value popped is {0} ", this.top.data);
+
+
+                this.top = this.top.next;
+
+
+            }
+
+
+
+
+
+
+
+            internal void IsEmpty()
+
+
+            {
+
+
+                while (this.top != null)
+
+
+                {
+
+
+                    Peek();
+
+
+                    Pop();
+
+
+                }
+
+
+                //this.top=null;
+
+
+            }
+
+
+
+
+
+
+
+        }
+
+
+
+    }
+
